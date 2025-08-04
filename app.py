f8f3c0d056d5eb1383ad3616cc5633c8835fb5ff
Anshita Dawar <anshitadawar90@gmail.com>
2025-08-04 16:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def books():
     return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,port=3000,host="0.0.0.0")
+
